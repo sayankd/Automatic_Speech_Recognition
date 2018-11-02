cfe8969a2fccd6201588fb970f8d8e74e0b8a7da
Sayan Das <sayankdas@outlook.com>
2018-11-02 10:35:25 +0530

    Delete train.py

--- Assignment1/train.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_hdf("./features/mfcc/timit.hdf")
-print(df.head())
-features = np.array(df["features"].tolist())
-labels = np.array(df["labels"].tolist())
-
-#Training code
